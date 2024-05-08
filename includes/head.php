2a5b996a0a073febda7b1cba460627551fe43fb3
Thefoxsnl <156410083+THEFOXSNL@users.noreply.github.com>
2024-05-08 22:26:56 +0200

    test

---
+++ includes/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>f1-net</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/b801578fa3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+// Include header section
+include("includes/head.php");
+?>
+
+<body>
+<?php
+// Include header section
+include("includes/header.php");
+
+include("includes/nav.php");
+?>
+
+<h1>Reizen</h1>
+</body>
+
+</html>
